cbd08bc029fb36e55c81839eeb327a078f58fb4a
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-05 12:28:34 -0400

    📦 NEW: Added the ElevationScroll code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,36 @@
 import React from "react";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { ReactComponent as Logo } from "../svg/logo.svg";
 
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 export default function Header() {
   return (
     <header>
-      <AppBar position="fixed">
-        <Toolbar>
-          <div className="logo">
-            <a href="/">
-              <Logo />
-            </a>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="fixed" color="transparent">
+          <Toolbar>
+            <div className="logo">
+              <a href="/">
+                <Logo />
+              </a>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </header>
   );
 }
