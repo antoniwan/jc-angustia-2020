2c28d29dad94829b5768af3676e6dceb05fdc567
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-05 21:51:08 -0400

    📦 NEW: CSS and footer work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { ReactComponent as Logo } from "../svg/logo.svg";
 import { ReactComponent as Twitter } from "../svg/icon-twitter.svg";
@@ -8,36 +7,34 @@ import { ReactComponent as Behance } from "../svg/icon-behance.svg";
 
 export default function Footer() {
   return (
-    <Container>
-      <footer>
-        <div>
-          <ul>
-            <li>
-              <a href="#twitter" rel="noopener noreferrer">
-                <Twitter />
-              </a>
-            </li>
-            <li>
-              <a href="#instagram" rel="noopener noreferrer">
-                <Instagram />
-              </a>
-            </li>
-            <li>
-              <a href="#behance" rel="noopener noreferrer">
-                <Behance />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <Typography variant="body2">
-            Copyright 2020 © Juan Angustia - All rights reserved.
-          </Typography>
-        </div>
-        <div className="logo">
-          <Logo />
-        </div>
-      </footer>
-    </Container>
+    <footer className="MuiToolbar-gutters">
+      <div>
+        <ul>
+          <li>
+            <a href="#twitter" rel="noopener noreferrer">
+              <Twitter />
+            </a>
+          </li>
+          <li>
+            <a href="#instagram" rel="noopener noreferrer">
+              <Instagram />
+            </a>
+          </li>
+          <li>
+            <a href="#behance" rel="noopener noreferrer">
+              <Behance />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Typography variant="body2">
+          Copyright 2020 © Juan Angustia - All rights reserved.
+        </Typography>
+      </div>
+      <div className="logo">
+        <Logo />
+      </div>
+    </footer>
   );
 }
