98b8e34bf909159783eb6daa3307d9c549b4a19c
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-07 18:22:03 -0400

    📦 NEW: Start form validation and submission process

--- src/components/LetsTalk.js
+++ src/components/LetsTalk.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import TextField from "@material-ui/core/TextField";
+import { ValidateEmail } from "../utils/email";
 
 export default function LetsTalk() {
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState();
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState();
+  const [message, setMessage] = useState("");
+
+  const handleChange = function (event) {
+    const { name, value } = event.target;
+    switch (name) {
+      case "name":
+        return setName(value);
+      case "email":
+        return setEmail(value);
+      case "message":
+        return setMessage(value);
+      default:
+        return false;
+    }
+  };
+
+  const handleFormSubmit = async function (e) {
+    const errors = await executeFormValidation();
+
+    if (!errors) {
+      setLoading(true);
+      // Send the form
+      //setLoading(false);
+      return;
+    }
+
+    return false;
+  };
+
+  const executeFormValidation = function () {
+    let foundError = false;
+    setNameError();
+    setEmailError();
+
+    if (name.length <= 0) {
+      setNameError(`Name can't be empty`);
+      foundError = true;
+    } else if (name.length <= 3) {
+      setNameError(`Name is too short`);
+      foundError = true;
+    }
+
+    if (email.length <= 0) {
+      setEmailError(`You must include an email`);
+      foundError = true;
+    } else if (!ValidateEmail(email)) {
+      setEmailError(`Please enter a valid email`);
+      foundError = true;
+    }
+
+    return foundError;
+  };
+
   return (
     <section>
       <Grid container spacing={0} justify="center">
@@ -22,42 +82,62 @@ export default function LetsTalk() {
       <Grid container spacing={0} justify="center">
         <Grid className="" item xs={12} md={4}>
           <form className="lets-talk-form" noValidate autoComplete="off">
-            <TextField
-              required
-              id="form-name"
-              label="Full name"
-              placeholder="Juan Doe"
-              variant="outlined"
-              fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
-            <TextField
-              required
-              id="form-email"
-              label="Email"
-              placeholder="juan.doe@gmail.com"
-              variant="outlined"
-              fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
-            <TextField
-              id="outlined-multiline-static"
-              label="Message"
-              multiline
-              rows={3}
-              variant="outlined"
-              fullWidth
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
+            {!loading ? (
+              <>
+                <TextField
+                  required
+                  name="name"
+                  id="form-name"
+                  label="Full name"
+                  placeholder="Juan Doe"
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  value={name.value}
+                  onChange={handleChange}
+                  helperText={nameError}
+                  error={!!nameError}
+                />
+                <TextField
+                  required
+                  name="email"
+                  id="form-email"
+                  label="Email"
+                  placeholder="juan.doe@gmail.com"
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  value={email.value}
+                  onChange={handleChange}
+                  helperText={emailError}
+                  error={!!emailError}
+                />
+                <TextField
+                  name="message"
+                  id="outlined-multiline-static"
+                  label="Message"
+                  multiline
+                  rows={3}
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  value={message}
+                  onChange={handleChange}
+                />
+              </>
+            ) : (
+              <CircularProgress size={50} />
+            )}
+
             <Button
               className="form-submit-button"
               variant="contained"
@@ -65,8 +145,10 @@ export default function LetsTalk() {
               disableElevation
               fullWidth
               size="large"
+              onClick={handleFormSubmit}
+              disabled={loading}
             >
-              Send message
+              {!loading ? `Send message` : `Sending...`}
             </Button>
           </form>
         </Grid>
