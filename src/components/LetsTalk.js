bb719d041f0c7020b934151a3ac7312b75b54148
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-12 22:43:22 -0400

    👌 IMPROVE: Image and SVG optimization pass and additional frontend improvements

--- src/components/LetsTalk.js
+++ src/components/LetsTalk.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
@@ -6,6 +6,121 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import TextField from "@material-ui/core/TextField";
 import Alert from "@material-ui/lab/Alert";
 import { ValidateEmail } from "../utils/email";
+import { ReactComponent as Polygon9 } from "../svg/particles/particle-Polygon-9.svg";
+import { ReactComponent as Polygon10 } from "../svg/particles/particle-Polygon-10.svg";
+import { ReactComponent as Polygon11 } from "../svg/particles/particle-Polygon-11.svg";
+import { ReactComponent as Vector4 } from "../svg/particles/particle-Vector-4.svg";
+import { ReactComponent as Group24 } from "../svg/particles/particle-Group-24.svg";
+
+import { ReactComponent as Ellipse9 } from "../svg/particles/particle-Ellipse-9.svg";
+import { ReactComponent as Ellipse10 } from "../svg/particles/particle-Ellipse-10.svg";
+import { ReactComponent as Ellipse11 } from "../svg/particles/particle-Ellipse-11.svg";
+import { ReactComponent as Group3 } from "../svg/particles/particle-Group-3.svg";
+import { ReactComponent as Group4 } from "../svg/particles/particle-Group-4.svg";
+import { ReactComponent as Group6 } from "../svg/particles/particle-Group-6.svg";
+import { ReactComponent as Group7 } from "../svg/particles/particle-Group-7.svg";
+import { ReactComponent as Group8 } from "../svg/particles/particle-Group-8.svg";
+import { ReactComponent as Polygon5 } from "../svg/particles/particle-Polygon-5.svg";
+import { ReactComponent as Rectangle18 } from "../svg/particles/particle-Rectangle-18.svg";
+import { ReactComponent as Rectangle19 } from "../svg/particles/particle-Rectangle-19.svg";
+import { ReactComponent as Rectangle21 } from "../svg/particles/particle-Rectangle-21.svg";
+import { ReactComponent as Vector1 } from "../svg/particles/particle-Vector-1.svg";
+import { ReactComponent as Vector2 } from "../svg/particles/particle-Vector-2.svg";
+
+const particles = [
+  {
+    name: "Polygon11",
+    src: Polygon11,
+    height: 55,
+    width: 55,
+    initialTop: 1210.78,
+    initialLeft: 380,
+    speed: 160,
+  },
+  {
+    name: "Polygon10",
+    src: Polygon10,
+    height: 27.32,
+    width: 27.32,
+    initialTop: 1622.73,
+    initialLeft: 311,
+    speed: 100,
+  },
+  {
+    name: "Polygon9",
+    src: Polygon9,
+    height: 55,
+    width: 55,
+    initialTop: 1600,
+    initialLeft: 1146,
+    speed: 50,
+    transform: `rotate(-26.51deg)`,
+  },
+  {
+    name: "Vector4",
+    src: Vector4,
+    height: 46,
+    width: 78,
+    initialTop: 1260.74,
+    initialLeft: 705,
+    speed: 40,
+    transform: `rotate(37.09deg)`,
+  },
+  {
+    name: "Group24",
+    src: Group24,
+    height: 64,
+    width: 64,
+    initialTop: 642,
+    initialLeft: 987,
+    speed: 7,
+  },
+];
+
+function TalkParticles() {
+  const Yoffset = 1140;
+  const Xoffset = 90;
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const handleScroll = function (e) {
+    const { scrollTop } = e.target.scrollingElement;
+    setScrollTop(scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <div className="talk-particles">
+      {particles.map((element) => {
+        const {
+          name,
+          height,
+          width,
+          initialTop,
+          initialLeft,
+          transform,
+          speed,
+        } = element;
+
+        return (
+          <element.src
+            key={name}
+            className={`talk-particle talk-particle-${name}`}
+            style={{
+              height: `${height}px`,
+              width: `${width}px`,
+              top: `${initialTop - Yoffset + scrollTop / speed}px`,
+              left: `${initialLeft - Xoffset}px`,
+              transform: `${transform}`,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
 
 export default function LetsTalk() {
   const [loading, setLoading] = useState(false);
@@ -85,6 +200,7 @@ export default function LetsTalk() {
 
   return (
     <section className="lets-talk">
+      <TalkParticles />
       <Grid container spacing={0} justify="center">
         <Grid className="" item xs={12} md={6}>
           <Typography variant="h2" id="hire-anchor" align="center" paragraph>
