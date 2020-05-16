3b7eb92a2cc75987ae7a2202548870ebe7950fde
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-15 20:54:31 -0400

    👌 IMPROVE: Adjust max-width accordint to the feedback. Closes #9

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -17,6 +17,15 @@ function createShadow(...px) {
 }
 
 let Theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1220,
+      xl: 1920,
+    },
+  },
   typography: {
     fontFamily: [
       "Lato",
