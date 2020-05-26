becd37874b753a56375c12bcf6d5ee422bc4daee
Antonio Rodriguez <antoniwan@gmail.com>
2020-05-26 16:18:56 -0400

    👌 IMPROVE: Improve the video parallax section (smoother)

--- src/components/MagicVideo.js
+++ src/components/MagicVideo.js
@@ -1,39 +1,18 @@
-import React, { useState, useEffect } from "react";
-import debounce from "lodash/debounce";
-import MagicVideoMp4 from "../video/loop-optim.mp4";
-import MagicVideoWebm from "../video/loop-optim.webm";
+import React, { useEffect } from "react";
+import { jarallax, jarallaxVideo } from "jarallax";
 
 export default function MagicVideo() {
-  const [scrollTop, setScrollTop] = useState(0);
-
-  const handleScroll = function (e) {
-    const { scrollTop } = e.target.scrollingElement;
-    setScrollTop(scrollTop);
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", debounce(handleScroll, 2));
+    jarallaxVideo();
+    jarallax(document.querySelectorAll(".jarallax"), {
+      speed: 0.2,
+    });
   });
 
   return (
-    <section className="magic-video">
-      <div className="magic-video-wrapper">
-        <video
-          className="video"
-          poster="img/stage.jpg"
-          playsInline={true}
-          autoPlay={true}
-          muted={true}
-          loop={true}
-          style={{
-            // top: `${820 + scrollTop / 20}px `,
-            transform: `translateY(-${scrollTop / 3.5}px)`,
-          }}
-        >
-          <source src={MagicVideoWebm} type="video/webm" />
-          <source src={MagicVideoMp4} type="video/mp4" />
-        </video>
-      </div>
-    </section>
+    <div
+      className="magic-video  jarallax"
+      data-video-src="https://www.youtube.com/watch?v=NL-AWrKqqb0&feature=youtu.be"
+    />
   );
 }
