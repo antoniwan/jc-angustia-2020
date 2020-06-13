356f8197cd69c1079dafba052068f16d97ba3da8
Antonio Rodriguez <antoniwan@gmail.com>
2020-06-13 15:20:59 -0400

    📦 NEW: Remove unused

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ScrollTop from "./components/ScrollTop";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import "./styles/App.css";
-// import TestingContent from "./components/TestingContent";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: "fixed",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-  offset: theme.mixins.toolbar,
-}));
+// const useStyles = makeStyles((theme) => ({
+
 
 function App() {
-  const classes = useStyles();
   return (
     <>
       <Header />
-      <div className={classes.offset} />
       <Home />
       <Footer />
-      {/* <ScrollTop /> */}
     </>
   );
 }
